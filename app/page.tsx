2b7938acfdfed7785d8cbfa63874062e697a72e7
Roni Savolainen <ronisvlnn@gmail.com>
2023-07-03 17:40:59 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -20,26 +20,23 @@ export default function Home() {
       setTasks(temp);
       setItem("");
     }
-
-
   };
-
 
   return (
     <div className='h-screen'>
-      <h1 className='flex justify-center text-2xl'>To Do</h1>
-      <div>
+      <h1 className='flex justify-center text-2xl p-4'>To Do</h1>
+      <div className='flex justify-center'>
         <input placeholder='Item name' value={item} onChange={(e) => {
           setItem(e.target.value);
         }} ></input>
         <button onClick={addItem}>Add item</button>
       </div>
-      <div>
+      <div className='flex justify-center py-4'>
         <ul>
           {tasks.map((task, index) => (
             <li key={index}>{task}<button onClick={() => {
               removeItem(task);
-            }} >Remove item</button></li>
+            }} className="ml-2 bg-red-500 text-white rounded-full p-2" >Remove item</button></li>
           ))}
         </ul>
       </div>
